a3f2be4f0ac3d898fc98ed919e7a0dd1ab5c43e0
Valentin Manaila <valentin.manaila@gmail.com>
2022-08-10 07:49:27 +0100

    next set-up

---
+++ pages/_app.tsx
@@ -0,0 +1,14 @@
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
+
+function MyApp({ Component, pageProps }: AppProps) {
+
+  return <ChakraProvider>
+    <Component {...pageProps} />;
+  </ChakraProvider>;
+  // return <Component {...pageProps} />;
+
+}
+
+export default MyApp;
