ab33c166a59da3aeffa607424061506218133f5c
Valentin Manaila <valentin.manaila@gmail.com>
2022-08-11 10:37:05 +0100

    wip

--- components/CardCreator.tsx
+++ components/CardCreator.tsx
@@ -12,7 +12,7 @@ type Props = {
 
 export const CardCreator: React.FC<Props> = ({ imageSrc, name, linkedInUrl, description }) => {
   return (
-    <Box color='gray.100' bg='tomato' borderRadius='2rem' w={600} minH={300}>
+    <Box color='gray.100' bg='gray.50' borderRadius='2rem' w={600} minH={300}>
       <Avatar size='xl' name={name} />
       <Box bg='brand.primary' borderRadius='50%' w={50} h={50} p={2}>
         <Icon as={FiLinkedin} w='100%' h='100%' />
